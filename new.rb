ee8ec3762862b835e919f2c792ffb086ae3f0a99
ZoraThomas <zt081203@wgcloud.org>
2018-06-19 20:01:43 +0000

    automatically backed up by learn

---
+++ new.rb
@@ -0,0 +1,13 @@
+def Wingardium_Leviosa
+  counter = 6
+  
+  while counter < 10
+  puts "Wingardium_Leviosa"
+  
+  counter = counter + 1
+  puts "{#counter} move(s) completed!"
+end
+
+end
+
+Wingardium_Leviosa
